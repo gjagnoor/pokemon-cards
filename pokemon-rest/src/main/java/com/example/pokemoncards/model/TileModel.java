56be2d0be50c6fd1a7b16efaea1c39cfaf8711eb
gjagnoor <gjagnoor@gmail.com>
2023-05-14 11:13:16 -0400

    feature: tile model

---
+++ pokemon-rest/src/main/java/com/example/pokemoncards/model/TileModel.java
@@ -0,0 +1,9 @@
+package com.example.pokemoncards.model;
+
+public class TileModel {
+    String name;
+    Integer position;
+    Boolean open;
+    String openURL;
+    String closeURL;
+}
--- pokemon-rest/src/main/resources/schema.sql
+++ pokemon-rest/src/main/resources/schema.sql
@@ -3,3 +3,32 @@ CREATE TABLE pokemon (
     name VARCHAR(255) UNIQUE NOT NULL,
     url VARCHAR(255) UNIQUE NOT NULL
 );
+
+CREATE TABLE user (
+    id INT PRIMARY KEY UNIQUE NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    logged_in BOOL NOT NULL
+);
+
+CREATE TABLE score (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    game_id INT PRIMARY KEY UNIQUE NOT NULL,
+    user1_id INT,
+    FOREIGN KEY (user1_id) REFERENCES user(id),
+    user2_id INT,
+    FOREIGN KEY (user2_id) REFERENCES user(id),
+    user1_score INT NOT NULL,
+    user2_score INT NOT NULL,
+    completed BOOL DEFAULT false,
+    winner_id INT,
+    FOREIGN KEY (winner_id) REFERENCES user(id),
+);
+
+CREATE TABLE game_state
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    game_id INT UNIQUE NOT NULL,
+    tiles ARRAY NOT NULL,
+);
+
